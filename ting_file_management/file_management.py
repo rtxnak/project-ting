e256e095a952c9fd8bd6cc6c26940b7fda4c0310
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-09-13 19:56:26 -0300

    feat: adicionou funcao txt_importer - requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith('.txt'):
+        try:
+            with open(path_file, mode='r',encoding='utf8') as f:
+                contents = f.read().splitlines()
+                return contents
+        except:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        print("Formato inválido", file=sys.stderr)
+
+
+# https://stackoverflow.com/questions/12330522/how-to-read-a-file-without-newlines
