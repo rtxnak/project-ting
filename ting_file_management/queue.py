5cad300c7b3f4e963a18ed0e00c93ad5ac29a7cc
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-09-13 19:04:06 -0300

    feat: adicionou classe Queue - requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,27 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self.size()
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self.is_empty():
+            return self._data.pop(0)
+        else:
+            return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= 0 and index < len(self._data): 
+            return self._data[index]
+        else:
+            raise IndexError
+    
+    def size(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return self._data == []
