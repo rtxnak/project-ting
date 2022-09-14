e81b5185b2f8a58271004fa6fe5b96d485fc6dca
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-09-14 16:09:54 -0300

    feat: adicionou funcao exists_word - requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for index in range(len(instance)):
+        occurrence = []
+        file = instance.search(index)
+        for index, line in enumerate(file['linhas_do_arquivo']):
+            if word.casefold() in line.casefold():
+                occurrence.append({'linha': index + 1})
+        if len(occurrence) > 0:
+            result.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occurrence
+            })
+    return result
+
+    # https://stackoverflow.com/questions/319426/how-do-i-do-a-case-insensitive-string-comparison
 
 
 def search_by_word(word, instance):
