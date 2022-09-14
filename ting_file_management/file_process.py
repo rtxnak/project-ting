6298d0dce8102a1764f2001b697e8b10eac0e42a
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-09-13 22:03:29 -0300

    feat: adicionou funcao process - requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,28 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    is_duplicate_file = False
+    # Checking if path_file is a duplicate
+    instance_length = instance.__len__()
+    for i in range(instance_length):
+        searched_dict = instance.search(i)
+        if path_file == searched_dict['nome_do_arquivo']:
+            is_duplicate_file = True
+
+    # Creating dictionary and enqueue on instance
+    if not is_duplicate_file:
+        txt_importation = txt_importer(path_file)
+
+        insert_dict = {
+                'nome_do_arquivo': path_file,
+                'qtd_linhas': len(txt_importation),
+                'linhas_do_arquivo': txt_importation
+        }
+
+        instance.enqueue(insert_dict)
+        print(f'{insert_dict}', file=sys.stdout)
 
 
 def remove(instance):
