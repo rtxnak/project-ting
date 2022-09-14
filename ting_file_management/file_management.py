e9d6accc3f05f3ed5a34ae69b13ec3ae02a2c777
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-09-13 22:15:18 -0300

    fix: correcao de erros de Linter

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,12 +1,13 @@
 import sys
 
+
 def txt_importer(path_file):
     if path_file.endswith('.txt'):
         try:
-            with open(path_file, mode='r',encoding='utf8') as f:
+            with open(path_file, mode='r', encoding='utf8') as f:
                 contents = f.read().splitlines()
                 return contents
-        except:
+        except Exception:
             print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
     else:
         print("Formato inválido", file=sys.stderr)
